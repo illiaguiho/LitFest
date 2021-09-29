703783de04e6785bad80eb40d7918fc042e55fd6
Elivi <89071360+illiaguiho@users.noreply.github.com>
2021-09-29 15:56:18 +0000

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+let resgistrationForm = document.getElementById('registration-form');
+let signInForm = document.getElementById('sign-in-form');
+let signInLink = document.getElementById('sign-in-link');
+let registerLink = document.getElementById('register-link');
+
+
+signInLink.addEventListener('click', function(){
+  if(signInForm.style.display = "none"){
+    signInForm.style.display = "block";
+    resgistrationForm.style.display = "none";
+  }
+});
+
+registerLink.addEventListener('click', function(){
+  if(signInForm.style.display != "none"){
+    signInForm.style.display = "none";
+    resgistrationForm.style.display = "block";
+  }
+});
